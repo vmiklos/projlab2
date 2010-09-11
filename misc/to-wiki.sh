0f5cc2892966a96f697a568c8838ea8ff600f29d
Miklos Vajna <vmiklos@frugalware.org>
2010-09-11 18:05:01 +0200

    1st week

---
+++ misc/to-wiki.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+docs="README"
+
+for i in $docs
+do
+	asciidoc -o $i.html $i
+done
+
+echo "okay?"
+read junk
+
+for i in $docs
+do
+	html2wiki --encoding UTF-8 --dialect MoinMoin $i.html >$i.wiki
+	sed -i '3i[[PageOutline]]\n' $i.wiki
+	kwrite --encoding UTF-8 $i.wiki
+done
