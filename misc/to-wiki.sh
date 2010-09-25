28f66b16318667949e08229d472f3cf319e97b54
Miklos Vajna <vmiklos@frugalware.org>
2010-09-25 17:26:04 +0200

    update

--- misc/to-wiki.sh
+++ misc/to-wiki.sh
@@ -14,5 +14,6 @@ for i in $docs
 do
 	html2wiki --encoding UTF-8 --dialect MoinMoin $i.html >$i.wiki
 	sed -i '3i[[PageOutline]]\n' $i.wiki
+	sed -i 's/\&gt;/>/g' $i.wiki
 	kwrite --encoding UTF-8 $i.wiki
 done
